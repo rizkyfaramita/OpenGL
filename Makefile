0e0cd3c5f0ca600962cb5f90aa6c4582b1effe32
rizkyfaramita <rizky.faramitha@gmail.com>
2018-04-03 14:42:32 +0700

    add initial 3dcar

--- Makefile
+++
@@ -1,6 +0,0 @@
-all: compile
-
-compile:
-	# g++ -Wall -o bin/car1 src/car1.cpp -lglut -lGLU -lGL -lm -lglfw
-	# g++ -Wall -o bin/car2 src/car2.cpp -lglut -lGLU -lGL -lm -lglfw -lGLEW	
-	g++ -Wall -o bin/car3 src/car3.cpp -lglut -lGLU -lGL -lm -lglfw -lGLEW
---
+++ triangle/t3.cpp
@@ -0,0 +1,85 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main( void )
+{
+   GLFWwindow *window;
+    
+   // Initialize the library
+   if (!glfwInit()) {
+        return -1;
+   }
+    
+   // Create a windowed mode window and its OpenGL context
+   window = glfwCreateWindow( SCREEN_WIDTH, SCREEN_HEIGHT, "T3 (Three-coloured Triangle)", NULL, NULL );    
+    
+   int screen_width, screen_height;
+   glfwGetFramebufferSize( window, &screen_width, &screen_height );
+
+   if (!window) {
+        glfwTerminate();
+        return -1;
+   }
+    
+   // Make the window's context current
+   glfwMakeContextCurrent(window);
+    
+   glViewport(0.0f, 0.0f, screen_width, screen_height); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+   glMatrixMode(GL_PROJECTION); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+   glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+   glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 600); // essentially set coordinate system
+   glMatrixMode(GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+   glLoadIdentity( ); // same as above comment
+    
+   GLfloat half_screen_width = SCREEN_WIDTH / 2;
+   GLfloat half_screen_height = SCREEN_HEIGHT / 2;
+    
+   GLfloat half_side_length = 250;
+    
+   GLfloat vertices[] = {
+        half_screen_width, half_screen_height + half_side_length, 0.0, // top center vertex
+        half_screen_width - half_side_length, half_screen_height - half_side_length, 0.0, // bottom left corner
+        half_screen_width + half_side_length, half_screen_height - half_side_length, 0.0 // bottom right corner
+   };
+    
+   GLfloat colour[] = {
+         255, 0, 0,
+         0, 255, 0,
+         0, 0, 255
+   };
+    
+   // Loop until the user closes the window
+   while (!glfwWindowShouldClose(window)) {
+      glClear(GL_COLOR_BUFFER_BIT);
+      // White background
+      glClearColor(0.0f, 0.0f, 0.0f, 0.0f);        
+      
+      // Render OpenGL here
+      glEnableClientState(GL_VERTEX_ARRAY); // tell OpenGL that you're using a vertex array for fixed-function attribute
+      glEnableClientState(GL_COLOR_ARRAY);
+      glVertexPointer(3, GL_FLOAT, 0, vertices); // point to the vertices to be used
+      glColorPointer(3, GL_FLOAT, 0, colour);
+      glDrawArrays(GL_TRIANGLES, 0, 3); // draw the vertixes
+      glDisableClientState(GL_COLOR_ARRAY);
+      glDisableClientState(GL_VERTEX_ARRAY); // tell OpenGL that you're finished using the vertex array attribute
+
+      // Swap front and back buffers
+      glfwSwapBuffers(window);
+
+      // Poll for and process events
+      glfwPollEvents();
+   }
+
+   glfwTerminate();
+    
+   return 0;
+}
+
+
+
+
